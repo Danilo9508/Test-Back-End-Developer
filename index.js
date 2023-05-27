fe3a7f97a56aa49f92b64dea4e11f02aa0b6eb4e
Danilo Zuñiga <da.zuniga@udla.edu.co>
2023-05-27 18:37:09 -0500

    test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const { PORT } = require('./src/utils/config');
 const server = require('./server');
 
-server.listen(PORT, async () => {
+server.listen(PORT, async () => { 
   console.log(`Listening on port ${PORT}`);
 });
